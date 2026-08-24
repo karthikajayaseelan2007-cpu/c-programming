a11dd1ce9a2a9e8addd9cfb6a97587eac6941cb6
Karthika <karthikajayaseelan2007@gmail.com>
2026-08-24 23:47:56 +0530

    Added s2.c fileclear

---
+++ s2.c
@@ -0,0 +1,35 @@
+/*
+Question:
+Write a C program to store book details
+(Title, Author, Price) using a structure.
+*/
+
+#include <stdio.h>
+
+struct Book
+{
+    char title[100];
+    char author[50];
+    float price;
+};
+
+int main()
+{
+    struct Book b;
+
+    printf("Enter Book Title: ");
+    scanf(" %[^\n]", b.title);
+
+    printf("Enter Author: ");
+    scanf(" %[^\n]", b.author);
+
+    printf("Enter Price: ");
+    scanf("%f", &b.price);
+
+    printf("\nBook Details\n");
+    printf("Title: %s\n", b.title);
+    printf("Author: %s\n", b.author);
+    printf("Price: %.2f\n", b.price);
+
+    return 0;
+}
