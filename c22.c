cd4a722c016898e8148f6c99299b30e787ed9deb
Karthika <karthikajayaseelan2007@gmail.com>
2026-08-24 22:21:16 +0530

    Added c22.c file

---
+++ c22.c
@@ -0,0 +1,32 @@
+/*
+Question:
+Develop a C program to determine whether a given integer
+is a palindrome number. Display appropriate messages based
+on the result.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n, original, remainder, reverse = 0;
+
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while (n != 0)
+    {
+        remainder = n % 10;
+        reverse = reverse * 10 + remainder;
+        n = n / 10;
+    }
+
+    if (original == reverse)
+        printf("%d is a palindrome number.", original);
+    else
+        printf("%d is not a palindrome number.", original);
+
+    return 0;
+}
