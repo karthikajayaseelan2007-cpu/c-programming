56a44921f03294e6c05eeabb99ef176479c7c2ec
Karthika <karthikajayaseelan2007@gmail.com>
2026-08-24 22:31:47 +0530

    Added c25.c file

---
+++ c25.c
@@ -0,0 +1,34 @@
+/*
+Question:
+Write a program to generate the Fibonacci sequence up to N terms.
+Also calculate the sum of all generated Fibonacci numbers.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    int first = 0, second = 1, next;
+    int sum = 0;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci sequence:\n");
+
+    for (i = 1; i <= n; i++)
+    {
+        printf("%d ", first);
+
+        sum = sum + first;
+
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    printf("\nSum of Fibonacci numbers = %d", sum);
+
+    return 0;
+}
