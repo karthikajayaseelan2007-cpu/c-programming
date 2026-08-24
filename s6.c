a3d977eecf8c690a9f70e5bf8beca09596e799d2
Karthika <karthikajayaseelan2007@gmail.com>
2026-08-24 23:59:45 +0530

    Added s6.c file

---
+++ s6.c
@@ -0,0 +1,35 @@
+/*
+Question:
+Write a C program to store and display the details
+of a mobile phone (Brand, Model, Price).
+*/
+
+#include <stdio.h>
+
+struct Mobile
+{
+    char brand[50];
+    char model[50];
+    float price;
+};
+
+int main()
+{
+    struct Mobile m;
+
+    printf("Enter Brand: ");
+    scanf(" %[^\n]", m.brand);
+
+    printf("Enter Model: ");
+    scanf(" %[^\n]", m.model);
+
+    printf("Enter Price: ");
+    scanf("%f", &m.price);
+
+    printf("\nMobile Details\n");
+    printf("Brand: %s\n", m.brand);
+    printf("Model: %s\n", m.model);
+    printf("Price: %.2f\n", m.price);
+
+    return 0;
+}
