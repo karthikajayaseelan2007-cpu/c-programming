f770492f056d45033304b476bbc23a86860cbea8
Karthika <karthikajayaseelan2007@gmail.com>
2026-08-25 16:10:45 +0530

    Semaphore 3 with 5 threads

---
+++ se3.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+sem_t sem;
+
+void *thread_function(void *arg)
+{
+    int id = *(int *)arg;
+
+    sem_wait(&sem);
+
+    printf("Thread %d entered critical section\n", id);
+
+    sleep(2);
+
+    printf("Thread %d leaving critical section\n", id);
+
+    sem_post(&sem);
+
+    return NULL;
+}
+
+int main()
+{
+    pthread_t threads[5];
+    int id[5];
+
+    sem_init(&sem, 0, 3);
+
+    for (int i = 0; i < 5; i++)
+    {
+        id[i] = i + 1;
+        pthread_create(&threads[i], NULL, thread_function, &id[i]);
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+
+    sem_destroy(&sem);
+
+    return 0;
+}
